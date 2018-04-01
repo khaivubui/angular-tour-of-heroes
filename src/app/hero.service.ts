bc2fdef63d73da996f2396787ca435568ea07628
Khai Bui <khaivubui@gmail.com>
2018-03-31 19:49:00 -0700

    Set up fake API using HTTPClientModule

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,24 +1,32 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
 import { MessageService } from './message.service';
 
 @Injectable()
 export class HeroService {
+  private heroesUrl = 'api/heroes';
 
-  constructor(private messageService: MessageService) { }
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService,
+  ) { }
 
   getHeroes(): Observable<Hero[]> {
-    this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES);
+    this.log('HeroService: fetched heroes');
+    return this.http.get<Hero[]>(this.heroesUrl);
   }
 
   getHero(id: number): Observable<Hero> {
-    this.messageService.add(`HeroService: fetched hero with id ${id}`);
-    return of(HEROES.find(hero => hero.id === id));
+    this.log(`HeroService: fetched hero with id ${id}`);
+    return of({id: 1, name: 'Khai'});
+  }
+
+  private log(message: string) {
+    this.messageService.add(message);
   }
 
 }
