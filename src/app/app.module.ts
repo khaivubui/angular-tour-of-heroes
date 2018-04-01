eaf7e7bceb3aadfc63199e7f70bef61e255eba9b
Khai Bui <khaivubui@gmail.com>
2018-03-31 19:57:21 -0700

    Fix lint

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { HttpClientModule } from '@angular/common/http';
 
 
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { InMemoryDataService }  from './in-memory-data.service';
+import { InMemoryDataService } from './in-memory-data.service';
 
 
 import { AppComponent } from './app.component';
